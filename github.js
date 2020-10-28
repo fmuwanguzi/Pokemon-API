ed49cd12e09388d02bf6af5a8243a25f5c229c1c
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-10-28 11:36:15 -0400

    [added element from github js]

--- github.js
+++ github.js
@@ -1,11 +1,24 @@
+const container = document.querySelector('.container');
+//console.log(container);
+
+
 fetch('https://api.github.com/users/fmuwanguzi')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const felixObj ={
+        name: githubData.name,
+        username: githubData.login,
+        id: githubData.id
+    }
+    console.log(felixObj);
+    
+    const newElement = document.createElement('p');
+    newElement.textContent = felixObj.name;
+    //append new elemnt tp container
+    container.appendChild(newElement);
 })
 .catch(error => {
     console.log(error)
 })
-
